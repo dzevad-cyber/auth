d4dacaf6683205cc950398a7eb9d6cf95a5fcafa
dzevad-cyber <dzevad.del@gmail.com>
2025-12-10 18:39:42 +0100

    fix: fix types in appError

--- apps/server/src/services/errors/appError.ts
+++ apps/server/src/services/errors/appError.ts
@@ -1,27 +1,27 @@
-import { unknown, type ZodError } from 'zod';
+import { type ZodError } from 'zod';
 
 export class AppError extends Error {
   statusCode: number;
   status: string;
   isOperational: boolean;
-  error: Errors;
+  errors: Errors;
 
-  constructor(message: string, statusCode: number, error: Errors = {}) {
+  constructor(message: string, statusCode: number, error: Errors) {
     super(message);
 
     this.statusCode = statusCode;
     this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
     this.isOperational = true;
-    this.error = error;
+    this.errors = error;
 
     Error.captureStackTrace(this, this.constructor);
   }
 }
 
-type ErrorTypes = 'INVALIDATION_ERROR';
+type ErrorTypes = 'INVALIDATION_ERROR' | 'UNKNOWN';
 type Errors =
   | {
       type: ErrorTypes;
       error: ZodError;
     }
-  | {};
+  | undefined;
