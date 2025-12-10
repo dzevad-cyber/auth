8718925865db43a8e68f70429747640effe49347
dzevad-cyber <dzevad.del@gmail.com>
2025-12-10 18:45:17 +0100

    feat: add email to userSchema

--- apps/server/src/db/schema/user.ts
+++ apps/server/src/db/schema/user.ts
@@ -4,8 +4,9 @@ export const usersTable = p.pgTable('users', {
   id: p.serial('id').primaryKey(),
   first_name: p.varchar().notNull(),
   last_name: p.varchar().notNull(),
-  password: p.text().notNull(),
-  password_confirm: p.text().notNull(),
+  password: p.varchar().notNull(),
+  email: p.varchar().unique(),
+  password_confirm: p.varchar().notNull(),
   created_at: p.timestamp().defaultNow().notNull(),
   updated_at: p
     .timestamp('updated_at', { mode: 'date', precision: 3 })
