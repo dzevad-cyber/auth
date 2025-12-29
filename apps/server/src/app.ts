9510f20d6f066c3e7b93bea8e0044d765b04808f
dzevad-cyber <dzevad.del@gmail.com>
2025-12-29 16:13:17 +0100

    feat: install cookie-parser, put express.json middleware to be first middleware to run

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -3,16 +3,18 @@ import morgan from 'morgan';
 import cors from 'cors';
 import { globalErrorHandler } from './globals/middlewares/error.handler.middleware.ts';
 import { v1Router } from './api/v1.ts';
+import cookieParser from 'cookie-parser';
 
 const app: Application = express();
 
+app.use(express.json());
+app.use(cookieParser());
 app.use(
   cors({
     origin: ['http://localhost:3000'],
     credentials: true,
   }),
 );
-app.use(express.json());
 app.use(morgan('dev'));
 
 app.use('/api/v1', v1Router);
