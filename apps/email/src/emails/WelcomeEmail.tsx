306073e06e5daf6acea3894e5eeabc01c7a2e10c
dzevad-cyber <dzevad.del@gmail.com>
2026-01-06 23:59:52 +0100

    feat: refactor WelcomeEmail, test images in emails

--- apps/email/src/emails/WelcomeEmail.tsx
+++ apps/email/src/emails/WelcomeEmail.tsx
@@ -9,6 +9,8 @@ import {
   Section,
   Preview,
   Tailwind,
+  Hr,
+  Img,
 } from '@react-email/components';
 
 export const WelcomeEmail = ({ name }: Props) => {
@@ -16,16 +18,41 @@ export const WelcomeEmail = ({ name }: Props) => {
     <Html>
       <Head />
       <Tailwind>
-        <Preview>Welcome to our platform!</Preview>
-        <Body className="bg-amber-400">
-          <Container>
-            <Section>
-              <Text>Hi {name},</Text>
-              <Text>Welcome to our community! We are glad to have you.</Text>
+        <Body className="bg-white h-full w-full">
+          <Preview>
+            The sales intelligence platform that helps you uncover qualified
+            leads.
+          </Preview>
+          <Container className="mx-auto py-5 pb-12">
+            <Img
+              src={`https://placehold.co/600x400`}
+              width="170"
+              height="150"
+              alt="auth-logo"
+              className="mx-auto"
+            />
+            <Text className="text-[16px] leading-[26px]">Hi {name},</Text>
+            <Text className="text-[16px] leading-[26px]">
+              Welcome to Koala, the sales intelligence platform that helps you
+              uncover qualified leads and close deals faster.
+            </Text>
+            <Section className="text-center">
+              <Button
+                className="bg-[#5F51E8] rounded-[3px] text-white text-[16px] no-underline text-center block p-3"
+                href="http://localhost:3000"
+              >
+                Get started
+              </Button>
             </Section>
-            <Button>Hello</Button>
-            <Button href="http://localhost:3000">Go to</Button>
-            <Button href="http://localhost:3000">dummy</Button>
+            <Text className="text-[16px] leading-[26px]">
+              Best,
+              <br />
+              The Koala team
+            </Text>
+            <Hr className="border-[#cccccc] my-5" />
+            <Text className="text-[#8898aa] text-[12px]">
+              470 Noor Ave STE B #1148, South San Francisco, CA 94080
+            </Text>
           </Container>
         </Body>
       </Tailwind>
