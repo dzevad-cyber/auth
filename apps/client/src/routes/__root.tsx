c02fd10ab71e0096d572ef106af6a958bb139769
dzevad-cyber <dzevad.del@gmail.com>
2026-01-12 01:19:43 +0100

    feat(client): add duration prop to toaster

--- apps/client/src/routes/__root.tsx
+++ apps/client/src/routes/__root.tsx
@@ -2,8 +2,6 @@ import { Outlet, createRootRouteWithContext } from '@tanstack/react-router';
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools';
 import { TanStackDevtools } from '@tanstack/react-devtools';
 
-import Header from '../components/Header';
-
 import TanStackQueryDevtools from '../integrations/tanstack-query/devtools';
 
 import type { QueryClient } from '@tanstack/react-query';
@@ -29,7 +27,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
           TanStackQueryDevtools,
         ]}
       />
-      <Toaster />
+      <Toaster duration={2000} />
     </div>
   ),
 });
