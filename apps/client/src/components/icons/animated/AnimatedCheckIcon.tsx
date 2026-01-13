8975785e1ce14ff8e55a752588edb4ca7a87f165
dzevad-cyber <dzevad.del@gmail.com>
2026-01-13 23:11:10 +0100

    chore(ui/icons): change success animation icon

--- apps/client/src/components/icons/animated/AnimatedCheckIcon.tsx
+++ apps/client/src/components/icons/animated/AnimatedCheckIcon.tsx
@@ -10,24 +10,32 @@ export const AnimatedCheckIcon = ({
   return (
     <div className={cn(className)} {...props}>
       <svg
-        fill="none"
-        height={size}
-        stroke="currentColor"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        viewBox="0 0 24 24"
-        width={size}
         xmlns="http://www.w3.org/2000/svg"
+        height={size}
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
       >
+        <motion.circle
+          cx="12"
+          cy="12"
+          r="10"
+          initial={{ pathLength: 0, opacity: 0 }}
+          animate={{ pathLength: 1, opacity: 1 }}
+          transition={{ delay: 0.3, duration: 0.5, ease: 'easeOut' }}
+        ></motion.circle>
         <motion.path
           initial={{ pathLength: 0, opacity: 0 }}
           animate={{ pathLength: 1, opacity: 1 }}
           transition={{
-            delay: 0.3,
+            delay: 0.8,
             duration: 0.5,
+            ease: 'easeOut',
           }}
-          d="M4 12 9 17L20 6"
+          d="m9 12 2 2 4-4"
         />
       </svg>
     </div>
