0ff07edb05b15e3d3892bec29ca80b4fa31b8632
dzevad-cyber <dzevad.del@gmail.com>
2026-01-14 00:19:43 +0100

    test: test 3

--- apps/client/src/components/animation/containers/AnimationContainer.tsx
+++ apps/client/src/components/animation/containers/AnimationContainer.tsx
@@ -3,6 +3,7 @@ import { motion, type HTMLMotionProps } from 'motion/react';
 const AnimatedContainer: React.FC<AnimatedContainerProps> = (props) => {
   console.log('[ AnimationContainer.tsx - 4 ] - :', 'test');
   console.log('[ AnimationContainer.tsx - 4 ] - :', 'test 1');
+  console.log('[ AnimationContainer.tsx - 4 ] - :', 'test 1');
 
   return (
     <motion.div
