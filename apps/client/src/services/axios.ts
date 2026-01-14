5845d1e76ee7c1b7fd482ef3bd9a6c6998f6cac8
dzevad-cyber <dzevad.del@gmail.com>
2026-01-14 16:55:32 +0100

    feat(client/axios): create axios interceptor to send access token on a request

--- apps/client/src/services/axios.ts
+++ apps/client/src/services/axios.ts
@@ -1,5 +1,15 @@
-import axios from 'axios'
+import { useClientStore } from '@/client-store/client.store';
+import axios from 'axios';
 
 export const _axios = axios.create({
   baseURL: 'http://localhost:5000',
-})
+});
+
+_axios.interceptors.request.use((config) => {
+  const accessToken = useClientStore((state) => state.accessToken);
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config;
+});
