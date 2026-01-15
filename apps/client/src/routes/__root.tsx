11259d624a96960e76ad23f08081f126a317965d
dzevad-cyber <dzevad.del@gmail.com>
2026-01-15 16:21:10 +0100

    refactor(client/__routes.tsx): remove check for auth user in the root

--- apps/client/src/routes/__root.tsx
+++ apps/client/src/routes/__root.tsx
@@ -6,28 +6,12 @@ import TanStackQueryDevtools from '../integrations/tanstack-query/devtools';
 
 import type { QueryClient } from '@tanstack/react-query';
 import { Toaster } from 'sonner';
-import { queryClient } from '@/integrations/tanstack-query/root-provider';
-import { getRefreshToken } from '@/features/auth/refresh/queries/getRereshToken.query';
 
 export interface MyRouterContext {
   queryClient: QueryClient;
 }
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
-  beforeLoad: async () => {
-    try {
-      const data = await queryClient.ensureQueryData({
-        queryKey: ['refreshToken'],
-        queryFn: getRefreshToken,
-      });
-
-      if (data.accessToken) {
-        queryClient.setQueryData(['accessToken'], data.accessToken);
-      }
-    } catch (error) {
-      console.error('Error refreshing token:', error);
-    }
-  },
   component: () => (
     <div className="grid h-screen">
       <Outlet />
