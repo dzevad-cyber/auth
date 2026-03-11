dc976ab1b69bab7c1dcb548e3221b46b8e61469b
dzevad-cyber <dzevad.del@gmail.com>
2026-03-11 20:03:07 +0100

    feat(client): add base url env variable

--- apps/client/src/services/axios.ts
+++ apps/client/src/services/axios.ts
@@ -2,7 +2,7 @@ import { queryClient } from '@/integrations/tanstack-query/root-provider';
 import axios from 'axios';
 
 export const _axios = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: import.meta.env.VITE_BASE_URL_DEV,
   withCredentials: true,
 });
 
